4558e5071329e8dfe2954352cf94e8c7fcbce12f
Matheus Braz <matheus.braz14@gmail.com>
2023-05-07 18:55:39 -0300

    chapter 4 - XML Views

--- webapp/index.js
+++ webapp/index.js
@@ -1,8 +1,11 @@
 sap.ui.define([
-	"sap/m/Text"
-], (Text) => {
-    "use strict";
-	new Text({
-		text: "Hello World"
-	}).placeAt("content");
+	"sap/ui/core/mvc/XMLView"
+], (XMLView) => {
+	"use strict";
+
+	XMLView.create({
+		viewName: "sap.ui.demo.walkthrough.view.App"
+	}).then(function (oView) {
+		oView.placeAt("content");
+	});
 })
