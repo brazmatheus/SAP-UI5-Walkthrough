99e45f7be0578a37b0d689b9432ce26442ad6679
Matheus Braz <matheus.braz14@gmail.com>
2023-05-07 20:40:01 -0300

    Chapter 6 - Modules

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,11 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], (Controller, MessageToast) => {
     "use strict"
     return Controller.extend("sap.ui.demo.walkthrough", {
         onShowHello: () => {
-            alert("Hello Earth.")
+            MessageToast.show("Hello Earth")
         }
     })
 })
