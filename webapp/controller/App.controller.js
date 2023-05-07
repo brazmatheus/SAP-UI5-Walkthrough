c13b6134cfeb58735893490300c7c438b937bdcb
Matheus Braz <matheus.braz14@gmail.com>
2023-05-07 20:42:05 -0300

    Chapter 7 - JSON Model

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,20 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], (Controller, MessageToast) => {
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], (Controller, MessageToast, JSONModel) => {
     "use strict"
     return Controller.extend("sap.ui.demo.walkthrough", {
+        onInit : function () {
+           // set data model on view
+           var oData = {
+              recipient : {
+                 name : "Worlda"
+              }
+           };
+           var oModel = new JSONModel(oData);
+           this.getView().setModel(oModel);
+        },
         onShowHello: () => {
             MessageToast.show("Hello Earth")
         }
